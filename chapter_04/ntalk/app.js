e30186ba9c997c399ac696b8b9fdbe1c134b0b58
Rafael Pontello <rafapntllo@gmail.com>
2018-09-07 13:37:02 -0300

    Exercises from chapter 04

---
+++ chapter_04/ntalk/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const load    = require('express-load')
+
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + 'public'));
+
+load('models')
+    .then('controllers')
+    .then('routes')
+    .into(app);
+
+app.listen(3000, () => {
+    console.log('Ntalk no ar.');
+});
